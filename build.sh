54a43251db3e555ff5ff316558a052a66b1227b0
ian <me@iany.me>
2021-08-05 10:47:53 +0800

    fix dead links

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ target="$1"
 
 IFS=$'\n' files=($(ls *.md | grep -v README | sort -t . -k 1,1n -k 2,2n))
 
-pandoc --toc --css pandoc.css -s -o "$target" "${files[@]}"
+pandoc --toc --css pandoc.css --metadata title="..." -s -o "$target" "${files[@]}"
