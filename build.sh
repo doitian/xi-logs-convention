4e75e02045ef43b7cc703fcb268b78aa04ae069c
ian <me@iany.me>
2021-08-05 10:49:36 +0800

    fix title

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ target="$1"
 
 IFS=$'\n' files=($(ls *.md | grep -v README | sort -t . -k 1,1n -k 2,2n))
 
-pandoc --toc --css pandoc.css --metadata title="..." -s -o "$target" "${files[@]}"
+pandoc --toc --css pandoc.css --metadata title="日志规范" -s -o "$target" "${files[@]}"
